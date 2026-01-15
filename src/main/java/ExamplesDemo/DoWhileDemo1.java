93bb3c69c5d5db4c62323bffe9a4ac99e11f14c0
Balram Rajale <rajalebalram@gmail.com>
2026-01-15 16:16:35 +0530

    Do While Loop example

---
+++ src/main/java/ExamplesDemo/DoWhileDemo1.java
@@ -0,0 +1,15 @@
+package ExamplesDemo;
+
+public class DoWhileDemo1 {
+
+	public static void main(String[] args) {
+		int i = 1;
+
+		do {
+			System.out.print(i + " ");
+			i++;
+		} while (false);
+
+	}
+
+}
