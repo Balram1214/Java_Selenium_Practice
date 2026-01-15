d267f42f0b18dd2058628233e47b1ed945c2080c
Balram Rajale <rajalebalram@gmail.com>
2026-01-15 16:12:02 +0530

    While loop example

---
+++ src/main/java/ExamplesDemo/WhileLoopDemo1.java
@@ -0,0 +1,14 @@
+package ExamplesDemo;
+
+public class WhileLoopDemo1 {
+
+	public static void main(String[] args) {
+
+		int i = 11;
+		while(i<=10) {
+			System.out.print(i+" ");
+			i++;
+		}
+	}
+
+}
