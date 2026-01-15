4376923101c597557a6085ab1d69b22788132fe8
Balram Rajale <rajalebalram@gmail.com>
2026-01-15 16:15:42 +0530

    For Loop example

---
+++ src/main/java/ExamplesDemo/ForLoopDemo.java
@@ -0,0 +1,17 @@
+package ExamplesDemo;
+
+public class ForLoopDemo {
+
+	public static void main(String[] args) {
+		
+		for(int i=1; i<=10; i++) {
+			System.out.print(i+" ");
+		}
+		
+		// It will goes to infinite loop
+//		for( ; ; ) {
+//			System.out.println(1+" ");
+//		}
+	}
+
+}
