1cad7807a37950145e7ca4a506a1b511cdac86a9
Balram Rajale <rajalebalram@gmail.com>
2026-01-16 13:22:35 +0530

    Array use

---
+++ src/main/java/ExamplesDemo/ArrayDemo.java
@@ -0,0 +1,22 @@
+package ExamplesDemo;
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+		int arr[]; // Declaration
+
+		arr = new int[5]; // Initialization
+
+		int a[] = new int[10]; // Both in single line
+
+		arr[0] = 5;
+		arr[1] = 10;
+		arr[2] = 15;
+		arr[3] = 20;
+		arr[4] = 25;
+
+		System.out.println(arr[2]);
+
+	}
+
+}
